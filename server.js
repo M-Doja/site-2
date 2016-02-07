b74396324fa6a061a0066f6aa29963722dd8070b
M-Doja <micdoja@yahoo.com>
2016-02-06 18:13:25 -0800

    push to git

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ var smtpTransport = nodemailer.createTransport("SMTP",{
     secure: true,
     service: "Gmail",
     auth: {
-			user: "dojadeveloper@gmail.com",
-			pass: "Patalaska1979"
-			// user: process.env.User_Name,
-			// pass: process.env.User_Password
+			// user: "",
+			// pass: ""
+			user: process.env.User_Name,
+			pass: process.env.User_Password
     }
 });
 
